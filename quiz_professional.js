94775b674d194698a6ac6b81259aba2dc6ad4e6a
anilikarikatti <anilkarikatti333@gmail.com>
2022-07-10 04:25:49 +0530

    some changes in coding

--- quiz_professional.js
+++ quiz_professional.js
@@ -26,7 +26,6 @@
         }
     ];
 
-    console.log("anil");
     
     let input = document.querySelectorAll('.txt');
     let question = document.querySelector('#que');
@@ -34,146 +33,118 @@
 
     
     let question_number  = 0;
+
     function print_question(i){
+        let keys = Object.keys(questions[i])
 
-        
-        let keys =Object.keys(questions[i]);
-    
         for(let key of keys){
-
+           
             for(let inp of input){
                 // console.log(inp);
-                if(inp.id == key){
-                    // console.log(keys);
                 
+                if(inp.id == key){
+                   
                     if(inp.id == 'que'){
                         question.innerText = questions[i].que;
-                        // console.log(questions[i].que);
-                        
                         
                     }
-                    
-                    // console.log(questions[i][key]);
-                    
+                   
                     inp.value =questions[i][key];
                 }
                 
                 
             }
         }
-}
+    }
 
-    print_question(question_number);
-
+    let user_answers =[];
+    
 
     let buttons = document.querySelectorAll('button')
 
-    console.log(buttons);
+    // console.log(buttons);
 
 
     for(let button of buttons){
-        button.onclick=next_previous;
+        button.onclick=btn_clicked;
         
     }
 
-    function next_previous(e){
+
+    function btn_clicked(e){
         console.log(e);
 
         let innerText_val = e.target.innerText;
         
-        if(innerText_val == 'Next'){
-            if(question_number < questions.length-1){
-                let checked_key = check_answers(question_number);
-                // console.log(correct_answers);
-                
-                // console.log(question_number,questions.length);
-                
-                question_number+=1;
-                // console.log(checked_key);
-                
-                if(checked_key != undefined)
-                     checked_key.checked=false;
-                print_question(question_number);
-                
-            }
+        if(innerText_val == 'Next')
+            next();
+        else if(innerText_val == 'Previous')
+            previous()
+        else if(innerText_val == 'Submit')
+            submit();
            
-        }
-        else if(innerText_val == 'Previous'){
-            if(question_number > 0){
-                
-                // 
-                
-                question_number-=1;
-               
-                print_question(question_number);
-               let a =  document.querySelector(checked_keys[question_number]);
-                console.log(checked_keys[question_number]);
-                
-                for(let radio_btn of radio_buttons){
+        
+    }
 
+    
+    function next(){
+        if(question_number < questions.length-1){
+           
+            question_number+=1;
+            
+                
+                }
+                
+        else{
+            alert("questions are completed please click submit button")
+        }   
+
+
+            for(let radio of radio_buttons){
+                if(radio.checked){
+                    user_answers.push(radio.value);
+                    console.log(user_answers);
+                    radio.checked = false;
+                    
+                }
+                
+            } 
+            print_question(question_number);
+            
              
-                let ans  =checked_keys[question_number]
-
-                    if(ans === radio_btn.value)
-                        radio_btn.checked = true;
-                }   
-                
-
-            }
-        }
-
-        else if(innerText_val == "Submit"){
-            let checked_key = check_answers(question_number);
-            let conform_by_user  = confirm("sure");
-            
-            if(conform_by_user){
-                let correct_answers = verify_answers();
-                alert(` your score is ${correct_answers}`);
-
-                quizgame();
-               
-            }
-            
-        }
-        
-        
-        
     }
 
+    function previous(){
+        if(question_number > 0){
+            question_number-=1;
+            print_question(question_number);
 
-    
-    let correct_answers = 0;
-    var checked_keys = [];
-    
-    
+            user_answers.pop();
+             console.log(user_answers);
 
-    function check_answers(i){
-        for(let radio_btn of radio_buttons){
-
-            
-            if(radio_btn.checked){
-                
-                checked_keys[i]=radio_btn.value;
-                console.log(checked_keys);
-                return radio_btn;
-            }
-
-            
         }
     }
 
-    function verify_answers(){
-        for(let  i =0 ; i < questions.length ;i++){
-            if(checked_keys[i] == questions[i].ans)
-                correct_answers+=1;
-        }
-        return correct_answers;
+    function submit(){
+        verify();
     }
+
+
+    function verify(){
+
+    let answers  = questions.map(elem => elem.ans);
    
+    let correct_answers  = answers.filter((elem,index) => elem ==user_answers[index] )
+    alert (`congratulations you answered only ${correct_answers.length}  out of ${answers.length}`)
+    
+    }
     
 
+    function order(){
+        print_question(question_number);
 
-  
+    }
+  order()
 
 }
 window.onload = quizgame;
